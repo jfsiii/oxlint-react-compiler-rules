24a21bd659c79dfad294a29927ead3f6d4e15ce6
John Schulz <github.com@jfsiii.org>
2026-03-17 13:03:24 -0400

    Remove EffectTest.tsx — splitting into individual files

--- src/EffectTest.tsx
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from "react";
-
-export default function EffectTest() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    setCount(1);
-  });
-  return <div>{count}</div>;
-}
